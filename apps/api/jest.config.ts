35d8879b36ed7fb1e5516e4ec38c380150337ddc
Adrian-Hawkins <u21669831@tuks.co.za>
2023-06-14 23:15:13 +0200

    A working nest api!

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,17 +1,11 @@
 /* eslint-disable */
 export default {
-    displayName: 'api',
-    preset: '../../jest.preset.js',
-    globals: {
-      'ts-jest': {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-      },
-    },
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.[tj]s$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/apps/api',
-  };
-  
+  displayName: 'api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/api',
+};
